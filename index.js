2fd49280550d810e9d12440dd96dcadca9f831cd
unknown <mhkg979@gmail.com>
2022-02-15 13:58:43 +0630

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const authRoute = require('./route/auth.js');
+const authRoute = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
